13383c66e1c50cafb4af5d9b4775b20fdfa25f2f
jadyamorim <88472254+jadyamorim@users.noreply.github.com>
2026-09-07 16:39:07 -0700

    Update Docker.groovy

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -11,16 +11,23 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName) {
         script.echo "building the docker image..."
-        script.sh "docker build -t $imageName ."
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+            script.sh "docker push $imageName ."
         }
 
-    def dockerLogin() {
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
-        }
     }
+
+    /*def dockerLogin() {
+
+    }
+
+
 
     def dockerPush(String imageName) {
         script.sh "docker push $imageName"
     }
+
+     */
 }
